a0b4799e69f367d9ee8ddf313df78bd5b50ac438
Somorin Olusola <somorinolusola1@gmail.com>
2025-08-08 13:45:31 +0100

    first commit

---
+++ Escape_sequences.py
@@ -0,0 +1,21 @@
+# Escape sequences in print - "\n", "\t"
+print("Line1\nLine2")
+print("Item1\tItem2")
+print("He said, \"Hello\"")
+print('It\'s Python!')
+
+# Newline (\n)
+print("welcome to Python\nLet's learn together")
+
+# Tab (\t)
+print("Name\tAge\tLocation")
+print("Aisha\t25\tLagos")
+
+# Quotes inside string
+print("She said, \"Hello there!\"")
+print('It\'s a sunny day.')
+
+# Backslash
+print("File path: C: \\Users\\Aisha\\Documents")
+
+print("          F\n         FFF\n        FFFFF\n       FFFFFFF\n      FFFFFFFFF")
