bbbb69527d16a48a66d0df5518a3c42c4f8ef8b3
Somorin Olusola <somorinolusola1@gmail.com>
2025-08-26 15:33:19 +0100

    new learning update

--- Learning_python_week4/modularizing_code.py
+++ Learning_python_week4/modularizing_code.py
@@ -140,3 +140,125 @@ def add(a, b):
 # Function call
 result = add(4, 6)
 print("The sum is:", result)  # Note the output and compare it with that of print()
+
+
+#3. yield
+def count_up_to(n):
+    i = 1
+    while i <= n:
+        yield i  # pause and return i
+        i += 1
+
+# Using the generator
+for number in count_up_to(5):
+    print(number)
+
+
+# more on function arguments(Type of arguments)
+#1. Positional Arguments
+# These are the most common. The order matters: values are assigned to parameter in the same order as they appear.
+def introduce(name, track):
+    print("My name is", name)
+    print("I am learning", track, ".")
+
+# function call
+introduce("Ngozi", "AI Engineering")  # Correct order
+
+# Change the arrangement and watch the output
+introduce("AI Engineering", "Ngozi")  # Incorrect order, this will throw a semantic error.
+
+#2. Keyword Arguments
+# Here, you explicitly mention the parameter name when calling the function. Order doesn't matte, since python knows which value goes where.
+def introduce(name, track):
+    print("My name is", name)
+    print("I am learning", track)
+
+# Function call
+introduce(name = "Ngozi", track = "AI Engineering")
+# Change the arrangement and watch the output
+introduce(track = "AI Engineering", name = "Ngozi")  # Here you notice that order does not matter.
+
+#3. Default arguments 
+# Here, you can give parameters a default value. Even if no value is provided when calling, the default is used.
+def introduce(name, track = "AI Engineering"):
+    print("My name is", name)
+    print("I am learning", track)
+
+# Function call
+# Without specifying the default argument, but watch the output
+introduce("paul")
+# Specify the default argument, and watch the output
+introduce("Tunji Paul", track = "AI Development")
+
+#4. Varying length Argument
+# Sometimes we don’t know how many arguments will be passed. Python provides two special symbols(that is the asterisk)
+# single asterisk for non-keyword arguments or tuple(*args)
+# Double asterisk for keyword arguments or dictionary(**kwargs)
+
+#a. non-keyword(tuple)
+# Collects extra positional arguments into a tuple.
+def add_numbers(*args):
+    total = 0
+    for num in args:
+        total += num
+    print("sum:", total)
+
+# function call
+# take note of the output
+add_numbers(2, 4, 6)
+add_numbers(10, 20, 30 ,40, 50)
+
+#b. keyword argument(dictionary)
+# Collects extra keyword arguments into a dictionary.
+def student_details(**kwargs):
+    for key, value in kwargs.items():
+        print(key, ":", value)
+
+# Function call - Take note of the output
+student_details(name = "Peter", track ="AI Engineering", interest = "Block Chain")
+
+# Lets implement on full code
+
+# Define student profile function
+# Ensure to not the order of arrangement of the types of arguments used.
+# This is how to arrange it of you are using everything or some of the together
+
+def participant_profile(name, age, track = "AI Development", *skills, **extra_info):
+    """
+    Generate a profile for a bootcamp participant using different types of arguments.
+    """
+    profile = f"\n--- Bootcamp Participant Profile ---\n"
+    profile += f"Name: {name}\n"
+    profile += f"Age: {age}\n"
+    profile += f"Track: {track}\n"
+
+    # Skills (from *args)
+    if skills:
+        profile += "Skills: " + ", ".join(skills) + "\n"
+    else:
+        profile += "Skills: Not yet specified\n"
+    
+    # Extra info (from **kwargs)
+    if extra_info:
+        profile += "Additional Info:\n"
+        for key, value in extra_info.items():
+            profile += f" - {key.capitalize()}: {value}\n"
+
+    return profile 
+
+# ------- LEts test -------
+# Example1: Using only positional arguments
+print(participant_profile("Peter", 24))
+
+# Example2: Adding keyword/default argument
+print(participant_profile("Ridwan", 29, track = "AI Engineering"))
+
+# Example3: Adding variable-length positional arguments (*args)
+print(participant_profile("David", 27, "Data Science", "Python", "SQL", "Machine Learning"))
+
+# Example4: Adding variable-length keyword arguments (*kwargs)
+print(participant_profile(
+    "Sophia", 30, "Cybersecurity",
+    "Networking", "Ethical Hacking", "Python",
+    interest="Blockchain", city="Shagamu", phone="08123456789"
+))
